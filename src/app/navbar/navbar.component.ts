8d03611bc0e70401cca3d98a67f82dbba5a2d667
Hinfray Theo <hinfraytheo@gmail.com>
2016-11-21 17:03:14 +0100

    navbar component

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fght-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
